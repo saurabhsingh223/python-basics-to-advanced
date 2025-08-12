f85b0f8ba95949d581d4686c442eb9abba7fcd73
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2025-08-13 01:27:59 +0530

    Add Python_Basics

---
+++ Python_Basics/1_hello.py
@@ -0,0 +1,33 @@
+print("Hello World")
+
+# Single line comment
+
+'''
+Multi-line comment
+'''
+
+# Variables in Python
+a = 5
+print(a)
+
+b = 55
+print(b)
+
+a = 6.5 #we can assign float to a variable that is assigned as a integer
+print(a)
+
+c = True  # or False: these are boolean variables
+print(c)
+
+d = "Saurabh"
+print(d)
+
+#every thing in python is object
+
+#None DataType
+e = None
+print(e)
+
+thislist = ["apple", "banana", "orange", False, 33, True, "apple"]
+print(thislist)
+thislist2 = {}
