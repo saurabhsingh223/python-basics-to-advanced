5eb64ce76cf7677b42d2b6c01c1368b83286bf4e
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2025-08-15 22:25:29 +0530

    add 2_Operators.py
    file contains data about operators in python

---
+++ Python_Basics/2_Operators.py
@@ -0,0 +1,34 @@
+num1 = 10
+num2 = 3
+
+# Arithmatic Operators(+, -, *, /)
+
+print("Num1 + num2 is: ",num1 + num2)
+print("Num1 - num2 is: ",num1 - num2)
+print("Num1 * num2 is: ",num1 * num2)
+print("Num1 ** num2 is: ",num1 ** num2) # ** for exponential power
+print("Num1 / num2 is: ",num1 / num2)
+print("Num1 // num2 is: ",num1 // num2) # // for fload division, no decimal
+print("Num1 % num2 is: ",num1 % num2)
+
+# Assignment Operator
+a = 4
+a += 2 # or a -= 2
+print(a)
+
+# Comparison operators
+x = 8
+y = 6
+print(x>=y)
+print(x<=y)
+print(x!=y)
+
+z = 8
+print(x == z) #true
+
+# Logical Operators: and, or
+
+print(x == z and x<y)
+print(x == z or x>y)
+print(not(False))
+print(not(True))
