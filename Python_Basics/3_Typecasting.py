b5dc0cd4ad5a45f92bd8a927a28cb9e7b1f4f267
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2025-08-16 22:32:53 +0530

    Add 3_Typecasting.py

---
+++ Python_Basics/3_Typecasting.py
@@ -0,0 +1,7 @@
+f = "5"
+#we cannot concat integer to string in python, for solving this problem we use typecasting
+print(f)
+#print(f + 1) //error
+print(int(f) + 1) #here we converted string to int
+g = 5
+print(g)
