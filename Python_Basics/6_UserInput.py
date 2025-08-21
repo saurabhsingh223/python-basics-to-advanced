f3f073ae27c6283128c3bfaed9530d8e8a64c890
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2025-08-21 14:49:25 +0530

    Add 6_UserInput.py basic user input functionality
    Add 6_UserInput.py with basic user input functionality
    
    - Implemented code to handle user input
    - This will be used in further modules for processing

---
+++ Python_Basics/6_UserInput.py
@@ -0,0 +1,9 @@
+# name = input("Enter Name: ")
+# print(name)
+
+#write a python program to take a number from user as an input, and six to it
+
+num = input("Enter a number: ")
+print(type(num)) #python always take input as an string
+print("num: ", int(num)+2)
+
