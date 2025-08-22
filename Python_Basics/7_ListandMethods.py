7f4e2d86675223242d6126a4f219353a824d1251
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2025-08-22 09:28:51 +0530

    Add 7_ListandMethods.py  list and method practice
    Added Python file for learning lists and methods

---
+++ Python_Basics/7_ListandMethods.py
@@ -0,0 +1,20 @@
+#lists are mutable and we can change it
+
+l1 = [31,42,5,6,74, "saurabh"] #we can put multiple datatypes in list
+print(type(l1))
+print(l1)
+# l1[0] = 'saurabh' #tis is allowed in lists because they are mutable
+# print(l1)
+
+#mehods of list
+
+#l1.remove("saurabh")
+#print(l1.count(2)) #count apperarences
+#l1.sort() #sort method
+#l1.pop() #remove last element
+#l1.append(69) #add element to last position
+#l1.clear() #clear the list
+# l1.extend([2,7,9,0]) #extent the size of list
+# print(l1.index(6))
+# print(l1[0:4]) #slicing in list
+print(l1)
